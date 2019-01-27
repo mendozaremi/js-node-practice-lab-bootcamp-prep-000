1ccf88d5a6b79a63365eace9b0465b956e6d0688
remi mendoza <mendozaremi@gmail.com>
2019-01-27 18:38:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Remi Mendoza"
-var height = "74"
+var height = "81"
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
+console.log("Height+1:")
 console.log(height)
